c425f96143d4a2b23effe4dbe0c35f97d703c22f
Thomas Fritz <thomas.fritz@gematik.de>
2023-03-27 17:55:32 +0200

    release 1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("application")
     id("maven-publish")
     id("io.ktor.plugin") version "2.2.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
@@ -13,12 +14,17 @@ repositories {
     maven(url="https://repo.labor.gematik.de/repository/maven-public/")
 }
 group = "de.gematik.hoba"
-version = "1.0"
+version = "1.2"
 application {
     mainClass.set("MainApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 dependencies {
@@ -34,16 +40,6 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-}
-
-tasks.jar {
-    manifest {
-        attributes["Main-Class"] = "MainApplicationKt"
-    }
-    configurations["compileClasspath"].forEach { file: File ->
-        from(zipTree(file.absoluteFile))
-    }
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
 
 publishing {
